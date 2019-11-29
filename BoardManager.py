b28daf32e3cde5d421378153da7a2152d4686258
Lima <emil.lima@boehringer-ingelheim.com>
2019-11-29 15:30:16 -0600

    board manager base methodes

--- BoardManager.py
+++ BoardManager.py
@@ -1,11 +1,13 @@
 class BoardManager:
-        def __init__(self, rows = 1, cols = 1):
+    
+    def __init__(self, rows = 1, cols = 1):
         self.__rows = rows
         self.__cols = cols
+        self.__tiles = "-" * (rows * cols)
         
     @property
     def rows(self):
-        return self.__row
+        return self.__rows
     
     @rows.setter
     def rows(self, rows):
@@ -18,3 +20,26 @@ class BoardManager:
     @cols.setter
     def cols(self, cols):
         self.__cols = cols
+
+    def calculate_position(self, row, col):
+        return row * (self.__cols) + (col + 1)
+
+    def get_tile(self, row, col):
+        if self.__rows < row or self.__cols < col:
+            return None
+        return self.__tiles[self.calculate_position(row, col)]
+
+    def set_tile(self, row, col, letter):
+        if self.__rows < row or self.__cols < col:
+            return
+        p = self.calculate_position(row, col)
+        self.__tiles = self.__tiles[:p-1] + letter + self.__tiles[p:]
+
+    def get_tiles(self):
+        return self.__tiles
+
+    def set_tiles(self, tiles):
+        self.__tiles = tiles
+        padding = self.__rows * self.__cols - len(tiles)
+        self.__tiles = self.__tiles + ("-" * padding) 
+        
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hi")
+from BoardManager import BoardManager
+
+if __name__ == "__main__":
+    mb = BoardManager(4,4)
+    mb.set_tiles("12345678")
+    print(mb.get_tiles())
+    mb.set_tile(1,1, 'A')
+    print(mb.get_tiles())
