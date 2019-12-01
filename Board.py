0af9c1a56446b07acbfd8ef8da196ebadcfa472f
Emil Lima <elima@pop-os.localdomain>
2019-12-01 00:00:33 -0600

    set word on board

--- Board.py
+++ Board.py
@@ -53,7 +53,10 @@ class Board:
             print("")
 
     def inside(self, row, col):
-        return row < self.__rows and col < self.__cols
+        return row >= 0 and col >= 0 and row < self.__rows and col < self.__cols
+
+    def inside_pos(self, pos):
+        return pos >= 0 and pos < self.__rows * self.__cols
 
     def draw_tile(self, row, col):
         pass
@@ -63,3 +66,38 @@ class Board:
         for i in range(self.__rows * self.__cols):
             letter  = random.choice(self.__letters)
             self.__tiles += letter
+
+    def fixed(self, r, c):
+        return False
+
+    def moved_position(self, r, c, d):
+        dr = {0: -1, 1: 0, 2: 1, 3:  0}
+        dc = {0:  0, 1: 1, 2: 0, 3: -1}
+        return r + dr[d], c + dc[d]
+
+    def set_word(self, word):
+        # select start position
+        r = random.choice(range(self.__rows))
+        c = random.choice(range(self.__cols))
+        while self.fixed(r, c):
+            r = random.choice(range(self.__rows))
+            c = random.choice(range(self.__cols))
+        
+        # determine word letter positions
+        letter_positions = [(r,c)]
+        for _ in range(len(word)-1):            
+            while self.fixed(r, c) or (r,c) in letter_positions or not self.inside(r, c):
+                d = random.choice(range(4))
+                nr, nc = self.moved_position(r, c, d)
+                if not self.fixed(nr, nc) and (nr, nc) not in letter_positions and self.inside(nr, nc):
+                    r, c = nr, nc
+            letter_positions.append((r,c))
+
+        # set tiles
+        for i in range(len(word)):
+            self.set_tile(letter_positions[i][0],letter_positions[i][1], word[i])    
+                
+                
+
+        
+                
--- main.py
+++ main.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     b = TerminalBoard(4,4)
     b.fill_random()
     b.show()
-    print()  
-    b.set_tile(1,1, 'A')
+    print()
+    b.set_word('EMIL')
     b.show()
